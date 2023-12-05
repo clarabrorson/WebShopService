f6cb7bd94f88c7c38af9fb595eedc94f79fcd9fb
clarabrorson <88clabro@gafe.molndal.se>
2023-12-05 13:35:38 +0100

    AuthService + AuthController

--- src/main/java/com/Molndal/WebShopService/Controllers/AuthController.java
+++ src/main/java/com/Molndal/WebShopService/Controllers/AuthController.java
@@ -1,6 +1,8 @@
 package com.Molndal.WebShopService.Controllers;
 
 import com.Molndal.WebShopService.Models.User;
+import com.Molndal.WebShopService.Service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,13 +13,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/webshop/auth")
 public class AuthController {
 
+    @Autowired
+    private AuthService authService;
+
     @PostMapping("/register")
     private ResponseEntity registerUser(@RequestBody User user) {
 
+        return authService.register(payload.getUsername(), payload.getPassword());
     }
 
     @PostMapping("/login")
     private ResponseEntity<User> loginUser(@RequestBody User user) {
+
+        return authService.login(payload.getUsername(), payload.getPassword());
 
     }
 }
--- src/main/java/com/Molndal/WebShopService/Service/AuthService.java
+++ src/main/java/com/Molndal/WebShopService/Service/AuthService.java
@@ -2,16 +2,21 @@ package com.Molndal.WebShopService.Service;
 
 import com.Molndal.WebShopService.Models.Role;
 import com.Molndal.WebShopService.Repository.RoleRepository;
-import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-@Service
-@Transactional
-public class AuthService {
+import java.util.HashSet;
+import java.util.Set;
 
+@Service
+public class AuthService {
     @Autowired
     private UserRepository userRepository;
     @Autowired
@@ -20,5 +25,32 @@ public class AuthService {
     private PasswordEncoder passwordEncoder;
     @Autowired
     private AuthenticationManager authenticationManager;
+
+    public ResponseEntity<User> register(String username, String password) {
+        String encryptedPassword = passwordEncoder.encode(password);
+
+        Role userRole = roleRepository.findByAuthority("USER").get();
+
+        Set<Role> authorities = new HashSet<>();
+
+        authorities.add(userRole);
+
+        return ResponseEntity.ok(userRepository.save( new User(0L, username, encryptedPassword, authorities) ));
+    }
+
+    public ResponseEntity<LoginResponse> login (String username, String password) {
+        try{
+            Authentication auth = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(username, password)
+            );
+
+            String token = tokenService.generateJwt(auth);
+
+            return ResponseEntity.ok(new LoginResponse(userRepository.findByUsername(username).get(), token));
+
+        } catch(AuthenticationException e){
+            return ResponseEntity.ok(new LoginResponse(null, ""));
+        }
+    }
 
 }
