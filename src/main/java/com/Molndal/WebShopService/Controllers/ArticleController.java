e3334953fafb9bc64e94d3a3f2b56bc1e2e945db
Fredrik <91frerin@gafe.molndal.se>
2023-12-07 14:34:48 +0100

    flyttat logiken från ArticleController till ArticleService

--- src/main/java/com/Molndal/WebShopService/Controllers/ArticleController.java
+++ src/main/java/com/Molndal/WebShopService/Controllers/ArticleController.java
@@ -20,18 +20,13 @@ public class ArticleController {
     @Autowired
     private ArticleService articleService;
 
-    @Autowired
-    private ArticleRepository articleRepository;
-
-
-    //ALla ska kunna detta, även de som inte är inloggade
+    //Alla har tillgång till denna förfrågan, även de som inte är inloggade
     @GetMapping("")
     private ResponseEntity<List<Article>> getArticles() {
     return ResponseEntity.ok(articleService.getAllArticles());
-
     }
 
-    //Vem ska kunna detta?
+    //Inloggade users och admin-förfrågan
     @GetMapping("/{id}")
     private ResponseEntity<Article> getOneArticle(
             @PathVariable Long id
@@ -39,7 +34,7 @@ public class ArticleController {
         return ResponseEntity.ok(articleService.getOneArticle(id));
     }
 
-    //Endast admin ska kunna detta
+    //Endast admin-förfrågan
     @PostMapping("/admin")
     private ResponseEntity<Article> addArticle(
             @RequestBody Article article
@@ -47,32 +42,21 @@ public class ArticleController {
         return ResponseEntity.ok(articleService.addNewArticle(article));
     }
 
-    //Endast admin ska kunna detta
-    //Bör lägga till någon slags try-catch här också
+    //Endast admin-förfrågan
     @PatchMapping("/admin/{id}")
     private ResponseEntity<Article> updateArticle(
             @PathVariable Long id,
             @RequestBody Article articleDetails
     ) {
-        Article article = articleRepository.findById(id).get();
-        article.setName(articleDetails.getName());
-        article.setCost(articleDetails.getCost());
-        article.setDescription(articleDetails.getDescription());
-        return ResponseEntity.ok(articleService.updateArticle(article));
+        return ResponseEntity.ok(articleService.updateArticle(id ,articleDetails));
     }
 
-    //Endast admin ska kunna detta
-    //try-catchen verkar inte fungera så bra...Den returnerar "Artikeln har tagits bort framgångsrikt" oavsett om id:t finns i db eller inte.
+    //Endast admin-förfrågan
     @DeleteMapping("/admin/{id}")
     private ResponseEntity<String> deleteArticle(
         @PathVariable Long id
 ) {
-    try {
-        articleService.deleteOneArticle(id);
-        return ResponseEntity.ok("Artikeln har tagits bort framgångsrikt");
-    } catch (EmptyResultDataAccessException e) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Ingen artikel hittades med det angivna ID:et");
-    }
+    return ResponseEntity.ok(articleService.deleteOneArticle(id));
 }
 }
 
--- src/main/java/com/Molndal/WebShopService/Service/ArticleService.java
+++ src/main/java/com/Molndal/WebShopService/Service/ArticleService.java
@@ -3,6 +3,9 @@ package com.Molndal.WebShopService.Service;
 import com.Molndal.WebShopService.Models.Article;
 import com.Molndal.WebShopService.Repository.ArticleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -26,11 +29,28 @@ public class ArticleService {
         return articleRepository.save(article);
     }
 
-    public Article updateArticle(Article article) {
-        return articleRepository.save(article);
+    //Bör lägga till felhantering i denna metod
+    public Article updateArticle(Long id, Article articleDetails) {
+
+        //Hämtar ut den befintliga artikeln
+        Article existingArticle = getOneArticle(id);
+
+        //Uppdaterar den befintliga artikeln
+        existingArticle.setName(articleDetails.getName());
+        existingArticle.setCost(articleDetails.getCost());
+        existingArticle.setDescription(articleDetails.getDescription());
+
+        //Returnerar och sparar den uppdaterade artikeln
+        return articleRepository.save(existingArticle);
     }
 
-    public void deleteOneArticle(Long id) {
-        articleRepository.deleteById(id);
+    public String deleteOneArticle(Long id) {
+        if (articleRepository.existsById(id)) {
+            articleRepository.deleteById(id);
+            return "Article with id: " + id + " was deleted";
+        } else {
+            return "Article with id: " + id + " does not exist";
+        }
     }
 }
+
