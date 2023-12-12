2c253098448322bc20397e4bcd689133ac77c4dc
clarabrorson <88clabro@gafe.molndal.se>
2023-12-12 11:43:14 +0100

    Update CartController.java
    added javadocs

--- src/main/java/com/Molndal/WebShopService/Controllers/CartController.java
+++ src/main/java/com/Molndal/WebShopService/Controllers/CartController.java
@@ -9,26 +9,26 @@ import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-
+/**@author Clara Brorson
+ * This class is used to handle requests from the client to the API.
+ * The class is used to get, add, update and delete articles from the cart.
+ * The class is also used to get the cart for the current user.
+ * Depending on the role of the user, the user will have access to different parts of the API.
+ */
 @RestController
 @RequestMapping("/webshop/cart")
 public class CartController {
-
     @Autowired private CartService cartService;
     @Autowired private UserService userService;
 
     @GetMapping("")
     private Cart getCart() {
-
         return cartService.getCarts();
     }
-
     @GetMapping("/{id}")
     private Cart getCartById(@PathVariable Long id) {
-
         return cartService.getCartById(id);
     }
-
     @PostMapping("/{id}")
     private Cart addArticleToCart(@PathVariable Long id){
         User currentUser = userService.getCurrentUser();
@@ -37,9 +37,10 @@ public class CartController {
         return cartService.getCartForCurrentUser();
     }
 
-    //@RequestParam int quantity hämtar quantity från URL
-    //Exempel: http://localhost:8080/webshop/cart/1/articles/1?quantity=5
-    //quantity=5 är en parameter
+    /**
+     * This method is used to update the quantity of an article in the cart.
+     * @RequestParam int quantity is used to get the quantity from the url.
+     */
     @PatchMapping("/{cartId}/articles/{articleId}")
     public ResponseEntity<Cart> updateArticleCount(
             @PathVariable Long cartId,
@@ -48,6 +49,7 @@ public class CartController {
         Cart updatedCart = cartService.updateArticleCount(cartId, articleId, quantity);
         return ResponseEntity.ok(updatedCart);
     }
+    
     @DeleteMapping("/{cartId}/articles/{articleId}")
     public ResponseEntity<Cart> deleteArticleFromCart(
             @PathVariable Long cartId,
