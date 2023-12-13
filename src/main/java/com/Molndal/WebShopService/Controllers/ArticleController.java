717a25f56d536b8beed8377d37100843bead0515
Fredrik <91frerin@gafe.molndal.se>
2023-12-13 21:32:04 +0100

    javadocs i ArticleService

--- src/main/java/com/Molndal/WebShopService/Controllers/ArticleController.java
+++ src/main/java/com/Molndal/WebShopService/Controllers/ArticleController.java
@@ -13,10 +13,11 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 /**
- * @author fredrik
  * Denna klass används för att hantera förfrågningar till <a href="http://localhost:8081/webshop/articles">...</a> från klienten till API:et.
  * Klassen används för att hämta, lägga till, uppdatera och ta bort artiklar från databasen.
  * Beroende på användarens roll, kommer användaren att ha tillgång till olika delar av API:et.
+ *
+ * @author Fredrik
  */
 @RestController
 @RequestMapping("/webshop/articles")
@@ -29,6 +30,7 @@ public class ArticleController { //
     /**
      * Denna metod används för att hämta alla artiklar från databasen. Alla har tillgång till denna metod, även de som inte är inloggade
      * Endpoint: GET /webshop/articles
+     *
      * @return en lista med alla artiklar.
      */
     @GetMapping("")
@@ -40,6 +42,7 @@ public class ArticleController { //
     /**
      * Denna metod används för att hämta en artikel från databasen med ett specifikt id. Alla har tillgång till denna metod, även de som inte är inloggade
      * Endpoint: GET /webshop/articles/{id}
+     *
      * @param id är id:t för den artikel som ska hämtas.
      * @return en artikel med det specifika id:t.
      */
@@ -53,6 +56,7 @@ public class ArticleController { //
     /**
      * Denna metod används för att lägga till en artikel i databasen. Endast användare med rollen "ADMIN" har tillgång till denna metod.
      * Endpoint: POST /webshop/articles
+     *
      * @param article är den artikel som ska läggas till i databasen.
      * @return den artikel som har lagts till i databasen.
      */
@@ -66,6 +70,7 @@ public class ArticleController { //
     /**
      * Denna metod används för att uppdatera en artikel i databasen. Endast användare med rollen "ADMIN" har tillgång till denna metod.
      * Endpoint: PATCH /webshop/articles/{id}
+     *
      * @param id är id:t för den artikel som ska uppdateras.
      * @param articleDetails är den artikel som innehåller den nya informationen.
      * @return den uppdaterade artikeln.
@@ -81,6 +86,7 @@ public class ArticleController { //
     /**
      * Denna metod används för att ta bort en artikel från databasen. Endast användare med rollen "ADMIN" har tillgång till denna metod.
      * Endpoint: DELETE /webshop/articles/{id}
+     *
      * @param id är id:t för den artikel som ska tas bort.
      * @return ett meddelande som indikerar om artikeln har tagits bort eller inte.
      */
