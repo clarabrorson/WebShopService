8f153362658cbcc9bb0fd9e36d72549db651242b
Fredrik <91frerin@gafe.molndal.se>
2023-12-13 10:54:20 +0100

    Lade till artiklar i tabellen så att det finns några initiella artiklar när programmet startas för första gången

--- src/main/java/com/Molndal/WebShopService/WebShopServiceApplication.java
+++ src/main/java/com/Molndal/WebShopService/WebShopServiceApplication.java
@@ -47,10 +47,29 @@ public class WebShopServiceApplication {
 				User admin = new User(1L, "admin", passwordEncoder.encode("password"), roles);
 				userRepository.save(admin);
 
+				//Skapar några artiklar och sparar i databasen
+				Article article = new Article("Banan", 10, "En gul frukt", 50);
+				Article article2 = new Article("Äpple", 5, "En röd frukt", 20);
+				Article article3 = new Article("Apelsin", 7, "En orange frukt", 30);
+				Article article4 = new Article("Päron", 8, "En grön frukt", 40);
+				Article article5 = new Article("Kiwi", 9, "En brun frukt", 60);
+				Article article6 = new Article("Mango", 11, "En gul frukt", 70);
+				Article article7 = new Article("Ananas", 12, "En gul frukt", 80);
+				Article article8 = new Article("Vattenmelon", 13, "En grön frukt", 90);
+				Article article9 = new Article("Citron", 14, "En gul frukt", 100);
+				Article article10 = new Article("Papaya", 15, "En gul frukt", 110);
 
-				//Skapar en article och sparar den i databasen
-				Article article = new Article(1L, "Banan", 10, "En gul frukt", 50);
 				articleRepository.save(article);
+				articleRepository.save(article2);
+				articleRepository.save(article3);
+				articleRepository.save(article4);
+				articleRepository.save(article5);
+				articleRepository.save(article6);
+				articleRepository.save(article7);
+				articleRepository.save(article8);
+				articleRepository.save(article9);
+				articleRepository.save(article10);
+
 			};
 	}
 
